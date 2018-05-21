de2f2aa37391188f4993e3e649ed7aa607200b22
syw <@192.168.102.1>
2018-05-21 13:53:22 +0800

    第一次提交

---
+++ FirstGit/src/cn/syw/git/GitTest.java
@@ -0,0 +1,12 @@
+package cn.syw.git;
+/**
+ * Test¿‡
+ * @author ÀÔ—“Œ‰
+ *
+ */
+public class GitTest {
+	public static void main(String [] args){
+		firstGit fg=new firstGit();
+		fg.show();
+	}
+}
---
+++ FirstGit/src/cn/syw/git/firstGit.java
@@ -0,0 +1,11 @@
+package cn.syw.git;
+/**
+ * 将要提交的Git代码（Demo）
+ * @author 孙岩武
+ *
+ */
+public class firstGit {
+	public void show(){
+		System.out.println("Hello Git!");
+	}
+}
